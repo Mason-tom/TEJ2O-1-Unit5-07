6953f11d50fb9cff5917a91dd415d2d54ff0926b
Mason-tom <mason.tom@stu.ocsb.ca>
2025-10-23 13:26:10 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mason
+ * Created on: Sep 2025
+ * This program contols a servo
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    //move servo to 0 degrees 
+    robotbit.Servo(servoNumber1, 0)
+    basic.clearScreen()
+    basic.showString('0')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    //move servo to 180 degrees 
+    robotbit.Servo(servoNumber1, 180)
+    basic.clearScreen()
+    basic.showString('180a')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
